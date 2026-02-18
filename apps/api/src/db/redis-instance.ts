2698fa7f0cb3b2d9cdb9ce52a2a5fea30654a003
Jimmy Tran <tranlehaiquan@gmail.com>
2026-02-18 12:40:14 +0700

    up

--- apps/api/src/db/redis-instance.ts
+++ apps/api/src/db/redis-instance.ts
@@ -2,6 +2,7 @@ import { createClient } from "redis";
 
 const client = createClient({
   password: process.env.REDIS_PASSWORD || "redis_secure_password",
+  url: process.env.REDIS_URL,
 });
 client.on("ready", () => {
   console.log("[redis] standalone ready.");
