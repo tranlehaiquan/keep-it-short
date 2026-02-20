589e655764e2ca6dfa3b19017dcd5e2ef33c69a7
Jimmy Tran <tranlehaiquan@gmail.com>
2026-02-20 17:08:14 +0700

    up

--- apps/web/src/apis/client.ts
+++ apps/web/src/apis/client.ts
@@ -1,6 +1,10 @@
 import type { AppType } from "../../../api/src/server";
 import { hc } from "hono/client";
 
-const client = hc<AppType>("/");
+const client = hc<AppType>("/", {
+  init: {
+    credentials: "include",
+  },
+});
 
 export default client;
