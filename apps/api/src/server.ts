44f5cb25f6635a4dfac700e94614d598bcb293dd
Jimmy Tran <tranlehaiquan@gmail.com>
2026-02-21 15:36:46 +0700

    up

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,6 +1,8 @@
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { auth } from "./lib/auth.js"; // path to your auth file
 import "./db/redis-instance.js";
 import shortLink from "./routers/shortLink.js";
 
@@ -9,6 +11,8 @@ const app = new Hono();
 // Serve static assets from the React build
 app.use("/assets/*", serveStatic({ root: "../web/dist" }));
 app.get("/", serveStatic({ path: "../web/dist/index.html" }));
+
+app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
 
 const routes = app.route("/", shortLink);
 export type AppType = typeof routes;
